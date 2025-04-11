53dc15200ccd9d9e4f8373fe0a6f392c2d145b24
shashankmishra07 <iamshashankmishra2003@gmail.com>
2025-04-11 22:15:32 +0530

    11-4-25

--- logintask/src/App.jsx
+++ logintask/src/App.jsx
@@ -1,18 +1,10 @@
-import React from "react";
-
-import {Route , Routes } from 'react-router-dom'
-import Sign from './Sign' 
-// import Login from './Login'
-
 function App(){
     return(
         <>
         <Routes>
-            <Route index element ={Sign}/>
-            <Route path="/login" element={Login}/>
+            <Route index element= {<Sign/>}/>
+            <Route path="/login"
         </Routes>
         </>
     )
 }
-
-export default App
