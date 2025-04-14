4604e5b847f2fbad3e0d9751e73ec64df429acb1
shashankmishra07 <iamshashankmishra2003@gmail.com>
2025-04-14 14:22:17 +0530

    ok

--- logintask/src/App.jsx
+++ logintask/src/App.jsx
@@ -5,6 +5,10 @@ function App(){
             <Route index element= {<Sign/>}/>
             <Route path="/login "
         </Routes>
+        <Routes>
+            <Route index element= {<Sign/>}/>
+            <Route path="/login "
+        </Routes>
 
         
     
